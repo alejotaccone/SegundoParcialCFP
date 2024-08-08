9d0fc5624ef8f6fa86a493669286e48ddcfb37a6
alejotaccone <93670743+alejotaccone@users.noreply.github.com>
2024-08-08 20:10:31 -0300

    Add files via upload

---
+++ Alejo Taccone (1)/Alejo Taccone/Alejo Taccone/LibreriaDePersonajes/Heroe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaDePersonajes
+{
+    public class Heroe : Personaje
+    {
+        protected string alianza;
+
+        public Heroe(string nombreReal, string nombrePersonaje, string origen, string alianza)
+            :base(nombreReal, nombrePersonaje, origen)
+        {
+            this.alianza = alianza;
+        }
+
+        public override string MostrarDescripcion()
+        {
+            return $"{nombrePersonaje} es un heroe de la alianza: {alianza}";
+        }
+    }
+}
